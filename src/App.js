c9f70e2d6e80e6b12bf6624643991c9accccc953
prathmeshjagtap <65167693+prathmeshjagtap@users.noreply.github.com>
2020-07-28 15:46:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SearchMovies from './SearchMovies'
+
+function App() {
+  return (
+    <div className="container" >
+     <h1 className= "title">My Movie Search Using React</h1>
+     <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
